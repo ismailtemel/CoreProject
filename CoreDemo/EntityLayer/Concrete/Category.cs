cfa6ee736a6036fae7e2bb78ced13017a86e90c7
İsmail Temel <ismailtemel2000@gmail.com>
2022-03-15 18:38:51 +0300

    Use .Net Core

---
+++ CoreDemo/EntityLayer/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        [Key]
+        // Property tanımlarken;0.Access Modifier(Erişim Belirleyici) - 1.Değişken türü - 2.İsim {get set}
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
