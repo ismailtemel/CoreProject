7516ec68f0f5ee322ee10469ae2ca2eb95a5483b
İsmail Temel <ismailtemel2000@gmail.com>
2022-03-18 13:30:15 +0300

    CoreProject

---
+++ CoreDemo/CoreDemo/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // Aşağıdaki kodun tanımı CategoryManager sınıfından bir değer türetiyoruz.new "EfCategoryRepository()" bu kod ICategoryDal'ı karşılar.
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values); 
+        }
+    }
+}
