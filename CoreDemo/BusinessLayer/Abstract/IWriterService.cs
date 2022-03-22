c81b84de17228f67cf74c9fed79878088256f95e
İsmail Temel <ismailtemel2000@gmail.com>
2022-03-22 22:16:35 +0300

    Core Demo

---
+++ CoreDemo/BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        void WriterAdd(Writer writer);
+        //void CategoryDelete(Writer writer);
+        //void CategoryUpdate(Writer writer);
+        //List<Writer> GetList();
+        //Writer GetById(int id);
+    }
+}
