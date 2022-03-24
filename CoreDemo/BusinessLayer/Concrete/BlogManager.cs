fd79b5e85f6aa6583556c0ed179f840df7dbdeac
İsmail Temel <ismailtemel2000@gmail.com>
2022-03-24 22:08:38 +0300

    Core Project

--- CoreDemo/BusinessLayer/Concrete/BlogManager.cs
+++ CoreDemo/BusinessLayer/Concrete/BlogManager.cs
@@ -22,10 +22,13 @@ namespace BusinessLayer.Concrete
         {
             return _blogDal.GetListWithCategory();
         }
-
-        public Blog GetById(int id)
+        public List<Blog> GetListWithCategoryByWriterBm(int id)
         {
-            throw new NotImplementedException();
+            return _blogDal.GetListWithCategoryByWriter(id);
+        }
+        public Blog TGetById(int id)
+        {
+            return _blogDal.GetById(id);
         }
 
         public List<Blog> GetBlogByID(int id)
@@ -49,12 +52,12 @@ namespace BusinessLayer.Concrete
 
         public void TAdd(Blog t)
         {
-            throw new NotImplementedException();
+            _blogDal.Insert(t);
         }
 
         public void TDelete(Blog t)
         {
-            throw new NotImplementedException();
+            _blogDal.Delete(t);
         }
 
         public void TUpdate(Blog t)
--- CoreDemo/CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/CoreDemo/Controllers/BlogController.cs
@@ -1,6 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +26,64 @@ namespace CoreDemo.Controllers
             ViewBag.i = id;
             var values = bm.GetBlogByID(id);
             return View(values);
-        }  
+        }
+
+        public IActionResult BlogListByWriter()
+        {
+            var values = bm.GetListWithCategoryByWriterBm(1);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+            List<SelectListItem> categoryvalues = (from x in cm.GetList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+            ViewBag.cv = categoryvalues;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterID = 1;
+                bm.TAdd(p);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteBlog(int id)
+        {
+            var blogvalue = bm.TGetById(id);
+            bm.TDelete(blogvalue);
+            return RedirectToAction("BlogListByWriter");
+        }
+        [HttpGet]
+        public IActionResult EditBlog(int id)
+        {
+            var blogvalue = bm.TGetById(id);
+            return View(blogvalue);
+        }
+        [HttpPost]
+        public IActionResult EditBlog()
+        {
+            return RedirectToAction("BlogListByWriter");
+        }
     }
 }
