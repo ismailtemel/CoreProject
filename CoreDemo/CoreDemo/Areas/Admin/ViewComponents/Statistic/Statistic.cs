9bd4aa968fc68583ec8e83069540f2d454c758c2
İsmail Temel <ismailtemel2000@gmail.com>
2022-03-31 20:48:36 +0300

    Core Project

--- CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic.cs
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
-    public class Statistic
+    public class Statistic : ViewComponent
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            return View();
+        }
     }
 }
---
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic4 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
