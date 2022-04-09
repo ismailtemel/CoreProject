d3619d4d2250cc0fa655237442c89ed283acb0ff
İsmail Temel <ismailtemel2000@gmail.com>
2022-04-09 23:12:24 +0300

    Core Project
    Edits were made in the writer panel and admin panel

--- CoreDemo/CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreDemo/CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -7,19 +7,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CoreDemo.ViewComponents.Writer
+namespace CoreDemo.Areas.Admin.Controllers
 {
-    public class WriterMessageNotification : ViewComponent
+    [Area("Admin")]
+    public class AdminMessageController : Controller
     {
         Message2Manager mm = new Message2Manager(new EfMessage2Repository());
         Context c = new Context();
-        public IViewComponentResult Invoke()
+        public IActionResult Inbox()
         {
             var username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
             var values = mm.GetInboxListByWriter(writerID);
             return View(values);
+        }
+        public IActionResult Sendbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
+            var values = mm.GetSendboxListByWriter(writerID);
+            return View(values);
+        }
+        public IActionResult ComposeMessage()
+        {
+            return View(); 
         }
     }
 }
--- CoreDemo/CoreDemo/Controllers/MessageController.cs
+++ CoreDemo/CoreDemo/Controllers/MessageController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,19 +16,44 @@ namespace CoreDemo.Controllers
     {
         Message2Manager mm = new Message2Manager(new EfMessage2Repository());
         Context c = new Context();
-        public IActionResult Inbox()
+        public IActionResult InBox()
         {
-            
             var username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
             var values = mm.GetInboxListByWriter(writerID);
             return View(values);
         }
+        public IActionResult SendBox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
+            var values = mm.GetSendboxListByWriter(writerID);
+            return View(values);
+        }
         public IActionResult MessageDetails(int id)
         {
             var value = mm.TGetById(id);
             return View(value);
+        }
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendMessage(Message2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
+            p.SenderID = writerID;
+            p.ReceiverID = 2;
+            p.MessageStatus = true;
+            p.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            mm.TAdd(p);
+            return RedirectToAction("InBox");
         }
     }
 }
